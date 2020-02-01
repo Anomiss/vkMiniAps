4947d3b3f1d222c21b8a8b2d40153e297de38b28
Anomiss <uOTCWDAkoI>
2020-02-02 00:52:35 +0300

    Updates

---
+++ precache-manifest.a92ee2c747ca9707c61da068577b8a88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fea699469db16b6902227e07c5c58f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75cfe7b2194470c94c4f",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "93c57d4fcaa54ba0e04d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "75cfe7b2194470c94c4f",
+    "url": "./static/js/2.5d5a4ffe.chunk.js"
+  },
+  {
+    "revision": "93c57d4fcaa54ba0e04d",
+    "url": "./static/js/main.5df490ea.chunk.js"
+  },
+  {
+    "revision": "d2770e64416b25c0a025",
+    "url": "./static/js/runtime-main.77d53bbc.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
